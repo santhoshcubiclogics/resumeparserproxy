cc906c31e3f48c5442fc33aa94ba350bdee4dbbc
Santhosh Kashyap K S <santhosh.k@cubiclogics.com>
2024-04-12 16:31:02 +0530

    port updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-const port = 8000;
+const port = 3000;
 
 app.use(bodyParser.json({limit:'50mb'}));
 
