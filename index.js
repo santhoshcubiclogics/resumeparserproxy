a8bcdf8290bd46b55c95b6062aec6ead3717d99d
Santhosh Kashyap K S <santhosh.k@cubiclogics.com>
2024-04-12 17:24:12 +0530

    update

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const url = 'http://149.137.246.7:47910/process_resume';
 app.post('/resume_parser', async (req, res) => {
     try {
         const { resume_keys, resume_text } = req.body;
-        const response = await axios.post(url, {
-            resume_keys,
-            resume_text
-        });
-        res.json({ data: response.data });
+        // const response = await axios.post(url, {
+        //     resume_keys,
+        //     resume_text
+        // });
+        res.json({ data: "response.data" });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
